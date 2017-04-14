0c48d6cbc20f6f7a8b4f49c5b0403746d8ba9905
Julie Pierre <julie.pierre3@gmail.com>
2017-04-14 15:15:11 +0200

    page d'accueil wip

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Atelierjus
     config.generators do |generate|
       generate.assets false
     end
-
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
